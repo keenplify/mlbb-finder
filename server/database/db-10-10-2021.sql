ace72b681919911f67204e5347931398a0298fe4
Aczell Bien Florencio <32163395+keenplify@users.noreply.github.com>
2021-10-11 11:02:58 +0800

    fix db

---
+++ server/database/db-10-10-2021.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 10, 2021 at 03:23 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mlbb_finder`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_friend`
+--
+
+CREATE TABLE `tbl_friend` (
+  `id` int(11) NOT NULL,
+  `friendUserId` int(11) NOT NULL,
+  `createdBy` int(11) NOT NULL,
+  `isAccepted` tinyint(1) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mlbbdata`
+--
+
+CREATE TABLE `tbl_mlbbdata` (
+  `id` int(11) NOT NULL,
+  `createdBy` int(11) NOT NULL,
+  `mlid` varchar(32) NOT NULL,
+  `ign` varchar(32) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_queue`
+--
+
+CREATE TABLE `tbl_queue` (
+  `id` int(11) NOT NULL,
+  `createdby` int(11) NOT NULL,
+  `preferences` varchar(500) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tickets`
+--
+
+CREATE TABLE `tbl_tickets` (
+  `id` int(11) NOT NULL,
+  `createdBy` int(11) NOT NULL,
+  `title` varchar(64) NOT NULL,
+  `body` varchar(1024) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(32) NOT NULL,
+  `lastname` varchar(32) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `birthday` date NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id`, `firstname`, `lastname`, `email`, `username`, `password`, `birthday`, `createdAt`, `updatedAt`) VALUES
+(5, 'Aczell', 'Florencio', 'keenplify@gmail.com', 'keenplify', '0', '2002-02-02', '2021-10-10 13:20:10', '2021-10-10 13:20:10');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_friend`
+--
+ALTER TABLE `tbl_friend`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_mlbbdata`
+--
+ALTER TABLE `tbl_mlbbdata`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_queue`
+--
+ALTER TABLE `tbl_queue`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_tickets`
+--
+ALTER TABLE `tbl_tickets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_friend`
+--
+ALTER TABLE `tbl_friend`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_mlbbdata`
+--
+ALTER TABLE `tbl_mlbbdata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_queue`
+--
+ALTER TABLE `tbl_queue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_tickets`
+--
+ALTER TABLE `tbl_tickets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
