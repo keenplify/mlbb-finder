ac4a9be07d7263b57ff7148a528eb88eb352dbd2
comethalley <kenneth.bonita07@gmail.com>
2021-10-19 23:25:29 +0800

    friend

--- server/api/friends/index.php
+++ server/api/friends/index.php
@@ -1,5 +1,29 @@
 <?php
+require_once("../../config/db.php");
+
+if (
+  isset($_POST['createdBy']) &&
+  isset($_POST['friendUserId']) 
+) {
+  $createdBy = filter_var($_POST['createdBy']);
+  $friendUserId = filter_var($_POST['friendUserId']);
+  
+  
+  $sql = "
+    INSERT INTO `tbl_friend` (friendUserId, createdBy, isAccepted)
+    VALUES ('$friendUserId', '$createdBy', false);
+  ";
 
 
-//If all statements are wrong, Return error 400
-http_response_code(400);
+  $res = $mysqli->query($sql);
+
+  if ($res) {
+    echo "Friend added successfully";
+  } else {
+    print_r($mysqli->error);
+    http_response_code(400);
+  }
+} else {
+  //If all statements are wrong, Return error 400
+  http_response_code(400);
+}
