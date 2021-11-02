66129426cbba03dd3b470bc0de11e617352609b9
Aczell Bien Florencio <32163395+keenplify@users.noreply.github.com>
2021-11-02 20:48:26 +0800

    database_update

--- database/mlbb_finder_nov_2.sql
+++ database/mlbb_finder_nov_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2021 at 05:05 PM
+-- Generation Time: Nov 02, 2021 at 01:47 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -54,13 +54,40 @@ CREATE TABLE `tbl_mlbbdata` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_preference`
+--
+
+CREATE TABLE `tbl_preference` (
+  `preference_id` int(11) NOT NULL,
+  `primaryRole` enum('Tank','Fighter','Marksman','Mage','Assassin','Support') NOT NULL,
+  `secondaryRole` enum('Tank','Fighter','Marksman','Mage','Assassin','Support') NOT NULL,
+  `gameMode` enum('Classic','Ranked','Brawl') NOT NULL,
+  `createdBy` int(11) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_preference`
+--
+
+INSERT INTO `tbl_preference` (`preference_id`, `primaryRole`, `secondaryRole`, `gameMode`, `createdBy`, `createdAt`) VALUES
+(1, 'Support', 'Assassin', 'Classic', 1, '2021-10-21 16:28:59'),
+(2, 'Mage', 'Assassin', 'Classic', 1, '2021-10-21 16:28:59'),
+(3, 'Assassin', 'Tank', 'Ranked', 1, '2021-10-21 16:28:59'),
+(4, 'Tank', 'Tank', 'Classic', 1, '2021-10-21 16:28:59'),
+(5, 'Marksman', 'Tank', 'Classic', 7, '2021-10-21 16:28:59'),
+(6, 'Mage', 'Tank', 'Classic', 7, '2021-10-21 16:28:59');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_queue`
 --
 
 CREATE TABLE `tbl_queue` (
   `queue_id` int(11) NOT NULL,
-  `createdby` int(11) NOT NULL,
-  `preferences` varchar(500) NOT NULL,
+  `createdBy` int(11) NOT NULL,
+  `preferenceId` int(11) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -103,7 +130,8 @@ CREATE TABLE `tbl_users` (
 --
 
 INSERT INTO `tbl_users` (`user_id`, `firstname`, `lastname`, `email`, `username`, `password`, `birthday`, `createdAt`, `updatedAt`) VALUES
-(6, 'Aczell', 'Florencio', 'keenplify@gmail.com', 'keenplify', '$2y$10$mu5BfRHSlOWn6tAYw9ONTuu84d5IAU9YtzODKQt5BtfLQ3yYUMIQ6', '2002-02-02', '2021-10-10 13:24:19', '2021-10-10 13:24:19');
+(7, 'Test', 'User', 'testuser@gmail.com', 'testuser', '$2y$10$AS/1aK3epaE3.1d4Wv8AAu3bsTkyNK7Vfr4b12D44kpqzf7g5ck6W', '0000-00-00', '2021-10-31 11:18:45', '2021-10-31 11:18:45'),
+(8, 'Aczell', 'Florencio', 'aczellbien.florencio2@gmail.com', 'keenplify', '$2y$10$Zy3HVEAAzZkGJyjQuFfjQuPgy5cd2hwT1UDv853LtcvyJk3L.gwJS', '2002-02-02', '2021-11-02 02:07:21', '2021-11-02 02:07:21');
 
 --
 -- Indexes for dumped tables
@@ -120,6 +148,12 @@ ALTER TABLE `tbl_friend`
 --
 ALTER TABLE `tbl_mlbbdata`
   ADD PRIMARY KEY (`data_id`);
+
+--
+-- Indexes for table `tbl_preference`
+--
+ALTER TABLE `tbl_preference`
+  ADD PRIMARY KEY (`preference_id`);
 
 --
 -- Indexes for table `tbl_queue`
@@ -158,10 +192,16 @@ ALTER TABLE `tbl_mlbbdata`
   MODIFY `data_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tbl_preference`
+--
+ALTER TABLE `tbl_preference`
+  MODIFY `preference_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `tbl_queue`
 --
 ALTER TABLE `tbl_queue`
-  MODIFY `queue_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `queue_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `tbl_tickets`
@@ -173,7 +213,7 @@ ALTER TABLE `tbl_tickets`
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
