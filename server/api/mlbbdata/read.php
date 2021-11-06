c02b73c15e4ecb95cb0f3508879bbb946ceaa994
benjeromebelena <benjerome.belena023@gmail.com>
2021-11-06 12:08:54 +0800

    mlbbdata (read & update)

--- server/api/mlbbdata/read.php
+++ server/api/mlbbdata/read.php
@@ -2,5 +2,27 @@
 
 contents: 
 mlbbdata_id
-kkkk
+
 output: json encoded mlbbdata. -->
+
+<?php
+require_once("../../config/db.php");
+
+if (isset($_GET['data_id'])) {
+    $data_id = filter_var($_GET['data_id']);
+
+    $sql = "SELECT * FROM 'tbl_mlbbdata' WHERE data_id = '$data_id'";
+
+    $result = $mysqli->query($sql);
+
+    if (!$result) {
+        echo "Data ID is invalid!";
+        http_response_code(406);
+        return;
+    }
+
+    echo json_encode($result->fetch_object("mlbbdata"))
+} else {
+    echo "Data ID not found!";
+    http_response_code(401);
+}
--- server/api/mlbbdata/update.php
+++ server/api/mlbbdata/update.php
@@ -6,3 +6,29 @@ mlid - string
 ign - string
 
 output: json encoded mlbbdata. -->
+
+<?php
+require_once("../../config/db.php");
+
+if (
+    isset($_POST['data_id']) &&
+    isset($_POST['mlid']) &&
+    isset($_POST['ign'])
+) {
+    $data_id = filter_var($_POST['data_id']);
+    $mlid = filter_var($_POST['mlid']);
+    $ign = filter_var($_POST['ign']);
+
+    $sql = "UPDATE 'tbl_mlbbdata' SET mlid = '$mlid', ign = '$ign' WHERE data_id = '$data_id'";
+
+    $result = $mysqli->query($sql);
+
+    if ($result) {
+        echo "MLBB Data updated successfully";
+    } else {
+        print_r($mysqli->error);
+        http_response_code(400);
+    }
+} else {
+    http_response_code(400);
+}
